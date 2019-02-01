60c18b8fa3c54fbae8eae6c409fd1e25c019e5be
임정주 <41723716+CraftJJGamer308@users.noreply.github.com>
2019-02-01 15:12:41 +0900

    1st version.

---
+++ baseConversion/script.js
@@ -0,0 +1,24 @@
+function f(){
+	var n, base, i, digit;
+    const num="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+	n=document.getElementById("n").value;
+    base=document.getElementById("base").value;
+    
+    document.getElementById("result").innerHTML="";
+
+	for(i=1;1;i++){
+		if (Math.pow(base,i)<=n && Math.pow(base,i+1)>n){
+			break; 
+		}
+	}
+	
+	for (;i>=0;i--){
+		digit=0;
+		while(n>=Math.pow(base,i)){
+			n-=Math.pow(base,i);
+			digit++;
+		}
+		document.getElementById("result").append(num[digit]);
+	}
+}
