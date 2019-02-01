dcc064d004e2c427b62c4b9dfa7e1668f68cd97e
임정주 <41723716+CraftJJGamer308@users.noreply.github.com>
2019-02-01 15:14:12 +0900

    Tabbed and Formatted Code.

--- baseConversion/script.js
+++ baseConversion/script.js
@@ -1,18 +1,18 @@
 function f(){
 	var n, base, i, digit;
-    const num="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    
+	const num="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 	n=document.getElementById("n").value;
-    base=document.getElementById("base").value;
-    
-    document.getElementById("result").innerHTML="";
+	base=document.getElementById("base").value;
+
+	document.getElementById("result").innerHTML="";
 
 	for(i=1;1;i++){
 		if (Math.pow(base,i)<=n && Math.pow(base,i+1)>n){
 			break; 
 		}
 	}
-	
+
 	for (;i>=0;i--){
 		digit=0;
 		while(n>=Math.pow(base,i)){
