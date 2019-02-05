092f677cce463e19e4fa5639940b007835c50748
임정주 <41723716+CraftJJGamer308@users.noreply.github.com>
2019-02-05 13:08:08 +0900

    added some details
    arranged positions of textboxes, added textbox hints(placeholders), added negative integer conversion

--- baseConversion/script.js
+++ baseConversion/script.js
@@ -1,22 +1,25 @@
-function f(){
+function f() {
 	var n, base, i, digit;
-	const num="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	const num = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-	n=document.getElementById("n").value;
-	base=document.getElementById("base").value;
-
-	document.getElementById("result").innerHTML="";
-
-	for(i=1;1;i++){
-		if (Math.pow(base,i)<=n && Math.pow(base,i+1)>n){
-			break; 
+	n = document.getElementById("n").value;
+	base = document.getElementById("base").value;
+	document.getElementById("result").innerHTML = "";
+	if (n<0) {
+		n=-n;
+		document.getElementById("result").append('-');
+	}
+	
+	for (i = 1; 1; i++) {
+		if (Math.pow(base, i) <= n && Math.pow(base, i + 1) > n) {
+			break;
 		}
 	}
 
-	for (;i>=0;i--){
-		digit=0;
-		while(n>=Math.pow(base,i)){
-			n-=Math.pow(base,i);
+	for (; i >= 0; i--) {
+		digit = 0;
+		while (n >= Math.pow(base, i)) {
+			n -= Math.pow(base, i);
 			digit++;
 		}
 		document.getElementById("result").append(num[digit]);
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 <h1>Welcome to the first repository of Kaiser Craft JJ</h1>
 <hr>
-Currently I have no knowledge of github, so please contact me with email,
+Currently I have no knowledge of github, so please contact me with email(mr4117@outlook.kr)
+
+This is a simple program which is able to convert a given integer into other bases(e.g. decimal, binary ...). <br>
+This utility is written with VANILLA JAVASCRIPT, and HTML. Ask me questions using the email above. <br>
+Thanks for visting!
