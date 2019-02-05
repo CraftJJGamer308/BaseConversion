640cedcb712d53df51a12f439465af1a61f545c4
임정주 <41723716+CraftJJGamer308@users.noreply.github.com>
2019-02-05 13:17:05 +0900

    formatted code

--- baseConversion/script.js
+++ baseConversion/script.js
@@ -5,11 +5,15 @@ function f() {
 	n = document.getElementById("n").value;
 	base = document.getElementById("base").value;
 	document.getElementById("result").innerHTML = "";
-	if (n<0) {
-		n=-n;
-		document.getElementById("result").append('-');
-	}
 	
+	if (n < 0) {
+		n = -n;
+		document.getElementById("result").append('-');
+	} else if (n == 0) {
+		document.getElementById("result").innerHTML = "0";
+		return;
+	}
+
 	for (i = 1; 1; i++) {
 		if (Math.pow(base, i) <= n && Math.pow(base, i + 1) > n) {
 			break;
